6d3d4ad60a276af45514d204b079342dc2a48f5c
akshay197 <70716828+akshay197@users.noreply.github.com>
2020-10-09 10:33:24 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,28 @@
-// JavaScript Document
+// JavaScript Document;
+var themeButton = document.querySelector("#toggletheme");
+var bodyElement = document.querySelector("body");
+var logo = document.querySelector("header img");
+var navbar = document.querySelector('header nav');
+var topnavbar = navbar.offsetTop;
+
+themeButton.addEventListener("click", function(){
+	var currentTheme = bodyElement.getAttribute("data-theme");
+	if (currentTheme === "light"){
+		bodyElement.setAttribute("data-theme", "dark");
+		logo.classList.add("logoBorder");
+	}
+	else{
+		bodyElement.setAttribute("data-theme", "light");
+		logo.classList.remove("logoBorder");
+	}
+});
+
+window.addEventListener("scroll", function(){
+	if (window.pageYOffset >= topnavbar){
+	navbar.classList.add("fixedNavbar") 
+	}
+	else{
+	navbar.classList.remove("fixedNavbar")
+	}
+})
+
