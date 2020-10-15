29db41f0d672f5b6d3224499267c51a120e6d00b
akshay197 <70716828+akshay197@users.noreply.github.com>
2020-10-15 15:36:29 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,15 @@
-// JavaScript Document;
+// JavaScript Document
 var themeButton = document.querySelector("#toggletheme");
 var bodyElement = document.querySelector("body");
 var logo = document.querySelector("header img");
 var navbar = document.querySelector('header nav');
+var mainElement = document.querySelector('main');
 var topnavbar = navbar.offsetTop;
 
-themeButton.addEventListener("click", function(){
-	var currentTheme = bodyElement.getAttribute("data-theme");
+themeButton.addEventListener("click", function(currentTheme){
+	currentTheme = bodyElement.getAttribute("data-theme");
+	localStorage.setItem('theme', currentTheme);
+
 	if (currentTheme === "light"){
 		bodyElement.setAttribute("data-theme", "dark");
 		logo.classList.add("logoBorder");
@@ -17,12 +20,25 @@ themeButton.addEventListener("click", function(){
 	}
 });
 
-window.addEventListener("scroll", function(){
-	if (window.pageYOffset >= topnavbar){
-	navbar.classList.add("fixedNavbar") 
-	}
-	else{
-	navbar.classList.remove("fixedNavbar")
+window.addEventListener('scroll', function(){
+	if (window.pageYOffset >= topnavbar) {
+		navbar.classList.add('fixedNavbar')
+		mainElement.classList.add('mainMargin');
+	} else {
+		navbar.classList.remove('fixedNavbar')
+		mainElement.classList.remove('mainMargin');
 	}
 })
 
+document.addEventListener('DOMContentLoaded', function(localTheme){
+	localTheme = localStorage.getItem('theme');	
+
+	if (localTheme === "light"){
+		bodyElement.setAttribute("data-theme", "dark");
+		logo.classList.add("logoBorder");
+	}
+	else{
+		bodyElement.setAttribute("data-theme", "light");
+		logo.classList.remove("logoBorder");
+	}
+})
